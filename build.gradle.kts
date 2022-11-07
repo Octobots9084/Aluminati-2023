afaf2c204802befc61d7f59f2d487eb3a52728f6
Anemone <87347339+Anniemone@users.noreply.github.com>
2022-11-07 12:38:19 -0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("org.rivierarobotics.gradlerioredux") version "0.9.7"
+}
+
+gradleRioRedux {
+    robotClass = "org.rivierarobotics.robot.Robot"
+    teamNumber = 5818
+    applyGradleRioConfiguration()
+}
---
+++ init.bat
@@ -0,0 +1,6 @@
+@echo off
+del /S /Q .git
+git init
+del README.md
+del init.sh
+del init.bat
