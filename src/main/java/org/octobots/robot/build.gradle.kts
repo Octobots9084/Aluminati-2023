c6746517b932513dd5c3e0feeeca37245d6722c6
Anemone <87347339+Anniemone@users.noreply.github.com>
2023-01-11 16:35:37 -0800

    Add files via upload

---
+++ src/main/java/org/octobots/robot/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("org.rivierarobotics.gradlerioredux") version "0.9.8"
+}
+
+gradleRioRedux {
+    robotClass = "org.octobots.robot.Robot"
+    teamNumber = 9084
+    applyGradleRioConfiguration()
+}
