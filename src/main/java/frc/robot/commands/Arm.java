7d6f64b092bd20195a175e2ca34154de0771664f
A. Southard <asouthard.sb@gmail.com>
2023-02-04 10:46:40 -0800

    California Girls 1.1 (feb 4, 10:46)

--- src/main/java/frc/robot/commands/Arm.java
+++ src/main/java/frc/robot/commands/Arm.java
@@ -1,25 +1,28 @@
 
 package frc.robot.commands;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel;
-import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
-
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.swerve.CaliGirls;
 
 public class Arm extends InstantCommand{
-    private double output;
+    //private double output; // This is used when using initialize() to control movement
     private CaliGirls caliGirls;
     public Arm(double output){
-        this.output = output;
+        //this.output = output; // This is used when using initialize() to control movement
         this.caliGirls = CaliGirls.getInstance();
         //addRequirements(caliGirls);
     }
-    @Override
+
+        /** The initialize function was used for the child snatcher.
+         * I have copied this method incase there is reson to use it
+         * The Arm currently uses moveToAngle(radians) and moveToAngleWitPID(radians)
+    */
+    /*@Override
     public void initialize(){
         caliGirls.setOutput(output);
-    }
+    }*/
+
+    // This method should move to target angle, but will likely overshoot slightly, and will pause the rest of the robot while running.
     public void moveToAngle(double angle){
         while(caliGirls.getRotation()<angle){
             caliGirls.setOutput(.2);
@@ -27,5 +30,12 @@ public class Arm extends InstantCommand{
         caliGirls.setOutput(0);
     }
 
-    
+    // This method should move to target angle better, and without pausing the robot.
+    // The PID coefficients are untuned.
+    // This Method will replace moveToAngle when it is funcitonal.
+    public void moveToAngleWithPID(double angle){
+        caliGirls.caliPIDControllerTop.setReference(angle, com.revrobotics.CANSparkMax.ControlType.kDutyCycle);
+        caliGirls.caliPIDControllerBottom.setReference(-angle, com.revrobotics.CANSparkMax.ControlType.kDutyCycle);
+    }
 }
+ 
