b988598adad2fc110e3be505fa2b81421fe136ef
Anemone <zombiegam3r2o06@gmail.com>
2023-02-11 00:46:01 -0800

    vscode staging issue

--- src/main/java/frc/robot/commands/TiltControl.java
+++ src/main/java/frc/robot/commands/TiltControl.java
@@ -1,11 +1,9 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.robot.ControlMap;
 import frc.robot.subsystems.arm.CaliGirls;
 
+//CURRENTLY BROKEN REWRITE BEFORE USE!
 public class TiltControl extends CommandBase{
     private CaliGirls caliGirls;
     public TiltControl() {
@@ -15,7 +13,6 @@ public class TiltControl extends CommandBase{
     @Override
     public void execute() {
         // caliGirls.setBottomPos(caliGirls.refrence + ControlMap.XBOX.getRightY() * 0.2 );
-        SmartDashboard.putNumber("pivot", caliGirls.caliEncoderBottom.getPosition());
     }
     
 }
--- src/main/java/frc/robot/subsystems/arm/CaliGirls.java
+++ src/main/java/frc/robot/subsystems/arm/CaliGirls.java
@@ -2,25 +2,18 @@
 package frc.robot.subsystems.arm;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkMaxAbsoluteEncoder;
-import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.robot.MotorIDs;
+import frc.robot.robot.Tuning;
+import frc.robot.util.MotorUtil;
 
 public class CaliGirls extends SubsystemBase{
-    private CANSparkMax motorTop;
-    private CANSparkMax motorBottom;
-    public SparkMaxPIDController caliPIDControllerTop;
-    private SparkMaxAbsoluteEncoder caliEncoderTop;
-    public double kPTop, kITop, kDTop, kIzTop, kFFTop, kMaxOutputTop, kMinOutputTop;
-    public SparkMaxPIDController caliPIDControllerBottom;
-    public SparkMaxAbsoluteEncoder caliEncoderBottom;
-    public double kPBottom, kIBottom, kDBottom, kIzBottom, kFFBottom, kMaxOutputBottom, kMinOutputBottom;
-    public double refrence = 0;
+    private CANSparkMax motorTop, motorBottom;
+    public double lastPosTop, lastPosBottom;
 
     private static CaliGirls caliGirls;
     public static CaliGirls getInstance(){
@@ -32,39 +25,27 @@ public class CaliGirls extends SubsystemBase{
 
     public CaliGirls() {
         this.motorTop = new CANSparkMax(MotorIDs.ARM_WRIST_ANGLE, MotorType.kBrushless);
+        motorTop.setSmartCurrentLimit(Tuning.CALI_TOP_STALL, Tuning.CALI_TOP_FREE);
+        MotorUtil.setupSmartMotion(
+            Type.kDutyCycle, 
+            Tuning.CALI_TOP_PID, 
+            Tuning.CALI_TOP_SM, 
+            Tuning.CALI_TOP_ENCODER_RESOLUTION, 
+            motorTop
+        );
+        
         this.motorBottom = new CANSparkMax(MotorIDs.ARM_PIVOT_ANGLE, MotorType.kBrushless);
-        this.caliEncoderTop = motorTop.getAbsoluteEncoder(Type.kDutyCycle);
-        this.caliEncoderBottom = motorBottom.getAbsoluteEncoder(Type.kDutyCycle);
-        this.caliPIDControllerTop = motorTop.getPIDController();
-        caliPIDControllerTop.setFeedbackDevice(caliEncoderTop);
-        kPTop = 0.1;
-        kITop = 0;
-        kDTop = 0;
-        kIzTop = 0;
-        kFFTop = 0;
-        kMaxOutputTop = 1;
-        kMinOutputTop = -1;
-        caliPIDControllerTop.setP(kPTop);
-        caliPIDControllerTop.setI(kITop);
-        caliPIDControllerTop.setD(kDTop);
-        caliPIDControllerTop.setIZone(kIzTop);
-        caliPIDControllerTop.setFF(kFFTop);
-        caliPIDControllerTop.setOutputRange(kMinOutputTop, kMaxOutputTop);
-        this.caliPIDControllerBottom = motorBottom.getPIDController();
-        caliPIDControllerBottom.setFeedbackDevice(caliEncoderBottom);
-        kPBottom = 8;
-        kIBottom = 0;
-        kDBottom = 0;
-        kIzBottom = 0;
-        kFFBottom = 0;
-        kMaxOutputBottom = 0.5;
-        kMinOutputBottom = -0.5;
-        caliPIDControllerBottom.setP(kPBottom);
-        caliPIDControllerBottom.setI(kIBottom);
-        caliPIDControllerBottom.setD(kDBottom);
-        caliPIDControllerBottom.setIZone(kIzBottom);
-        caliPIDControllerBottom.setFF(kFFBottom);
-        caliPIDControllerBottom.setOutputRange(kMinOutputBottom, kMaxOutputBottom);
+        motorTop.setSmartCurrentLimit(Tuning.CALI_BOTTOM_STALL, Tuning.CALI_BOTTOM_FREE);
+        MotorUtil.setupSmartMotion(
+            Type.kDutyCycle, 
+            Tuning.CALI_BOTTOM_PID, 
+            Tuning.CALI_BOTTOM_SM, 
+            Tuning.CALI_BOTTOM_ENCODER_RESOLUTION, 
+            motorBottom
+        );
+        
+        
+        
     }
 
 
@@ -73,16 +54,13 @@ public class CaliGirls extends SubsystemBase{
         motorBottom.set(-output);
     }
 
-    public void setBottomPos(double x) {
-        this.refrence = x;
-        caliPIDControllerBottom.setReference(x, ControlType.kPosition);
+    public void setBottomPos(double angle) {
+        motorBottom.getPIDController().setReference(angle, ControlType.kSmartMotion);
     }
 
-
-    public double getRotation(){
-        double rotTop = motorTop.getAbsoluteEncoder(Type.kDutyCycle).getPosition();
-        double rotBottom = motorBottom.getAbsoluteEncoder(Type.kDutyCycle).getPosition();
-        return (rotTop-rotBottom)/2;
+    
+    public void setTopPos(double angle) {
+        motorTop.getPIDController().setReference(angle, ControlType.kSmartMotion);
     }
 
 }
