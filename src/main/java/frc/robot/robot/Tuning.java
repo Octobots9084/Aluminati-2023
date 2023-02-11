4144e449f639bc67644bc81226c2d43599427a2d
Anemone <zombiegam3r2o06@gmail.com>
2023-02-11 01:35:02 -0800

    More refactors

--- src/main/java/frc/robot/robot/Tuning.java
+++ src/main/java/frc/robot/robot/Tuning.java
@@ -1,8 +1,11 @@
 package frc.robot.robot;
 
+import java.util.ArrayList;
+
 import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 
+import frc.robot.util.MotionMagicConfig;
 import frc.robot.util.PIDConfig;
 import frc.robot.util.SmartMotionConfig;
 
@@ -17,7 +20,24 @@ public class Tuning {
     //Extension
     public static final int EXTENSION_FREE = 10;
     public static final int EXTENSION_STALL = 12;
+    //Drive
+    public static final StatorCurrentLimitConfiguration DRIVE_STATOR_LIMIT = new StatorCurrentLimitConfiguration(
+        true,
+        60,
+        60,
+        0.05
+    );
     
+    public static final SupplyCurrentLimitConfiguration DRIVE_SUPPLY_LIMIT = new SupplyCurrentLimitConfiguration(
+        true,
+        63,
+        63,
+        0.05
+    );
+    //Turn
+    public static final int TURN_MOTOR_FREE = 30;
+    public static final int TURN_MOTOR_STALL = 31;
+
 
     //Movement Characteristics
     //Wrist
@@ -44,4 +64,30 @@ public class Tuning {
     public static final PIDConfig EXTENSION_PID = new PIDConfig(2, 0, 0);
     public static final double EXTENSION_MAX_OUT = 1;
     public static final double EXTENSION_MIN_OUT = -1;
+
+    //Drive Motors
+    public static final PIDConfig FL_DRIVE_PID = new PIDConfig(0.06, 0.0001, 0, 0.077);
+    public static final PIDConfig FR_DRIVE_PID = new PIDConfig(0.06, 0.0001, 0, 0.06);
+    public static final PIDConfig BL_DRIVE_PID = new PIDConfig(0.06, 0.0001, 0, 0.075);
+    public static final PIDConfig BR_DRIVE_PID = new PIDConfig(0.06, 0.0001, 0, 0.06);
+    public static final MotionMagicConfig DM_MM_CONFIG = new MotionMagicConfig(
+        new ArrayList<>(), true,
+        10000.0, 10000.0,
+        300, 2,
+        60, 10
+    );
+
+    //Turn Motors
+    public static final PIDConfig FL_TURN_PID = new PIDConfig(10, 0, 0);
+    public static final PIDConfig FR_TURN_PID = new PIDConfig(10, 0, 0);
+    public static final PIDConfig BL_TURN_PID = new PIDConfig(10, 0, 0);
+    public static final PIDConfig BR_TURN_PID = new PIDConfig(10, 0, 0);
+    public static final SmartMotionConfig TM_SM_CONFIG = new SmartMotionConfig(
+        true,
+        20000.0,
+        18000.0,
+        20000.0,
+        0.1
+    );
+
 }
