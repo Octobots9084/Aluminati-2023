a39f80e43f402a6b6aa3ab6f4a24dd597a1a7d5f
Anemone <zombiegam3r2o06@gmail.com>
2023-02-11 00:45:31 -0800

    error fixes :P

--- src/main/java/frc/robot/commands/Arm.java
+++ src/main/java/frc/robot/commands/Arm.java
@@ -8,35 +8,16 @@ import frc.robot.subsystems.arm.CaliGirls;
 public class Arm extends InstantCommand{
     //private double output; // This is used when using initialize() to control movement
     private CaliGirls caliGirls;
-
-
-        /** The initialize function was used for the child snatcher.
-         * I have copied this method incase there is reson to use it
-         * The Arm currently uses moveToAngle(radians) and moveToAngleWitPID(radians)
-    */
-    /*@Override
-    public void initialize(){
-        caliGirls.setOutput(output);
-    }*/
-
-    // This method should move to target angle, but will likely overshoot slightly, and will pause the rest of the robot while running.
-    public void moveToAngle(double angle){
-        while(caliGirls.getRotation()<angle){
-            caliGirls.setOutput(.2);
-        } 
-        caliGirls.setOutput(0);
+    public Arm() {
+        this.caliGirls = CaliGirls.getInstance();
     }
 
-    // This method should move to target angle better, and without pausing the robot.
-    // The PID coefficients are untuned.
-    // This Method will replace moveToAngle when it is funcitonal.
     @Override
     public void initialize() {
-        caliGirls = CaliGirls.getInstance();
         SmartDashboard.putNumber("Init", 1);
         double angle = 0;
-        caliGirls.caliPIDControllerTop.setReference(angle, com.revrobotics.CANSparkMax.ControlType.kPosition);
-        caliGirls.caliPIDControllerBottom.setReference(-angle, com.revrobotics.CANSparkMax.ControlType.kPosition);
+        caliGirls.setTopPos(angle);
+        caliGirls.setBottomPos(-angle);
     }
 }
  
