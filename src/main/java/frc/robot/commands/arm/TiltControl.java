081ff213e0bfbd31a5ff903183fcc842f7241a32
Aayush <aayushak178@gmail.com>
2023-02-11 16:22:07 -0800

    Exponential Joystick Arm Control and somewhat tuned the arm

--- src/main/java/frc/robot/commands/arm/TiltControl.java
+++ src/main/java/frc/robot/commands/arm/TiltControl.java
@@ -1,20 +1,21 @@
 package frc.robot.commands.arm;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.robot.ControlMap;
 import frc.robot.subsystems.arm.CaliGirls;
 
 //CURRENTLY BROKEN REWRITE BEFORE USE!
-public class TiltControl extends CommandBase{
+public class TiltControl extends CommandBase {
     private CaliGirls caliGirls;
+
     public TiltControl() {
         this.caliGirls = CaliGirls.getInstance();
         addRequirements(caliGirls);
     }
+
     @Override
     public void execute() {
-        caliGirls.setBottomPos(caliGirls.lastPosBottom + 0.01 * ControlMap.XBOX.getRightY());
+        caliGirls.setBottomPos(caliGirls.lastPosBottom + 0.0025 * Math.pow(ControlMap.XBOX.getRightY(), 3));
     }
-    
+
 }
