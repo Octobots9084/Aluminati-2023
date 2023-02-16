cdc889c8e64978c4d27a14d32460f2bdbd79a9a1
Coolkid0629 <kaustubh0629@gmail.com>
2023-02-15 18:22:24 -0800

    ButtonConfig.java

--- src/main/java/frc/robot/robot/ButtonConfig.java
+++ src/main/java/frc/robot/robot/ButtonConfig.java
@@ -1,67 +1,77 @@
 package frc.robot.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.arm.ArmExtensionPos;
+import frc.robot.commands.arm.GoToFull;
 import frc.robot.commands.arm.SetArmAngle;
 import frc.robot.commands.arm.SetWristAngle;
+import frc.robot.commands.swerve.SetDriverAssist;
+import frc.robot.commands.swerve.ZeroGyro;
+import frc.robot.subsystems.arm.ArmPositions;
+import frc.robot.subsystems.arm.IntakeClaws;
 
 public class ButtonConfig {
     public void initTeleop() {
         // DRIVER LEFT
 
-        // new JoystickButton(ControlMap.DRIVER_BUTTONS, 13)
-        //     .whileTrue(new SetDriverAssist(true));
+        //Zero Gyro
+        new JoystickButton(ControlMap.DRIVER_BUTTONS, 1)
+                .onTrue(new ZeroGyro());
+        //Driver Assist
+        new JoystickButton(ControlMap.DRIVER_BUTTONS, 13)
+                .whileTrue(new SetDriverAssist(true));
+        //Stow
+        new JoystickButton(ControlMap.DRIVER_BUTTONS, 8)
+                .onTrue(new GoToFull(ArmPositions.STORE));
+        //Intake
+        new JoystickButton(ControlMap.DRIVER_LEFT, 1)
+                .onTrue(null);
+        //Eject
+        new JoystickButton(ControlMap.DRIVER_LEFT, 2)
+                .onTrue(null);
+        //Floor Pickup Align
+        new JoystickButton(ControlMap.DRIVER_RIGHT, 2)
+                .onTrue(null);
+        //Floor Pickup Collect
+        new JoystickButton(ControlMap.DRIVER_RIGHT, 2)
+                .onTrue(null);
 
-        // new JoystickButton(ControlMap.DRIVER_BUTTONS, 13)
-        //     .whileFalse
-        //     (new SetDriverAssist(false));
+        //////END DRIVER 1//////////////////////////
 
-        // // new JoystickButton(ControlMap.DRIVER_LEFT, 1)
-        // //     .onTrue(new RotateTo(0));
+        //////CO DRIVER////////////////
 
-        // new JoystickButton(ControlMap.DRIVER_BUTTONS, 11)
-        //     .onTrue(new ZeroGyro());
+        //Substation Deploy
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 1)
+                .onTrue(null);
+        //Substation Collect
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 4)
+                .onTrue(null);
+        //Stow
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 6)
+                .onTrue(new GoToFull(ArmPositions.STORE));
+        //Cone Place High
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 7)
+                .onTrue(null);
+        //Cube Place High
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 8)
+                .onTrue(null);
+        //Cone Place Mid
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 9)
+                .onTrue(null);
+        //Cube Place Mid
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 10)
+                .onTrue(null);
+        //Cone Place Low
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 11)
+                .onTrue(null);
+        //Cube Place Low
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 12)
+                .onTrue(null);
+        //Arm Override Enable
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 13)
+                .whileTrue(null);
 
-        // new JoystickButton(ControlMap.DRIVER_BUTTONS, 10) 
-        //     .onTrue(new BalanceChargeStation(DriveTrain.getInstance(), Gyro.getInstance()));
-
-        // new JoystickButton(ControlMap.DRIVER_BUTTONS, 9) 
-        // .onTrue(new driveToPos(new Pose2d(14, 0.7, new Rotation2d(0))));
-
-        // Xbox A Button
-        new JoystickButton(ControlMap.XBOX, 1)
-                .onTrue(new SetWristAngle(0.268));
-        new JoystickButton(ControlMap.XBOX, 1)
-                .onTrue(new SetArmAngle(0.758));
-        new JoystickButton(ControlMap.XBOX, 1)
-                .onTrue(new ArmExtensionPos(1091));
-
-        // Xbox B Button
-
-        new JoystickButton(ControlMap.XBOX, 2)
-                .onTrue(new SetWristAngle(0.457));
-        new JoystickButton(ControlMap.XBOX, 2)
-                .onTrue(new SetArmAngle(0.586));
-        new JoystickButton(ControlMap.XBOX, 2)
-                .onTrue(new ArmExtensionPos(183));
-
-        // Xbox X  Button
-        new JoystickButton(ControlMap.XBOX, 3)
-                .onTrue(new SetWristAngle(0.344));
-
-        // Xbox Y Button
-
-        new JoystickButton(ControlMap.XBOX, 4)
-                .onTrue(new SetWristAngle(0.256));
-        new JoystickButton(ControlMap.XBOX, 4)
-                .onTrue(new SetArmAngle(0.783));
-        new JoystickButton(ControlMap.XBOX, 4)
-                .onTrue(new ArmExtensionPos(3400));
-        // new JoystickButton(ControlMap.DRIVER_BUTTONS, 5) 
-        //     .onTrue(new ClawsTest());
-        // new JoystickButton(ControlMap.XBOX, 4)
-        // .onTrue(new SetArmAngle(0.875));
-        // new JoystickButton(ControlMap.XBOX, 2).onTrue(new ArmZero());
-
+        //////END CO-DRIVER//////////////////////////
     }
 }
