a9d72c1c235993197134fa30159e9da5f9b8362b
Coolkid0629 <kaustubh0629@gmail.com>
2023-02-15 19:33:41 -0800

    NEW BUTTON CONFIG FOR BOTH DRIVERS

--- src/main/java/frc/robot/robot/DriverButtonConfig.java
+++ src/main/java/frc/robot/robot/DriverButtonConfig.java
@@ -1,7 +1,77 @@
 package frc.robot.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.arm.ArmExtensionPos;
+import frc.robot.commands.arm.GoToFull;
+import frc.robot.commands.arm.SetArmAngle;
+import frc.robot.commands.arm.SetWristAngle;
+import frc.robot.commands.swerve.SetDriverAssist;
+import frc.robot.commands.swerve.ZeroGyro;
+import frc.robot.subsystems.arm.ArmPositions;
+import frc.robot.subsystems.arm.IntakeClaws;
+
 public class DriverButtonConfig {
     public void initTeleop() {
-        
+        // DRIVER LEFT
+
+        //Zero Gyro
+        new JoystickButton(ControlMap.DRIVER_BUTTONS, 1)
+                .onTrue(new ZeroGyro());
+        //Driver Assist
+        new JoystickButton(ControlMap.DRIVER_BUTTONS, 13)
+                .whileTrue(new SetDriverAssist(true));
+        //Stow
+        new JoystickButton(ControlMap.DRIVER_BUTTONS, 8)
+                .onTrue(new GoToFull(ArmPositions.STORE));
+        //Intake
+        new JoystickButton(ControlMap.DRIVER_LEFT, 1)
+                .onTrue(null);
+        //Eject
+        new JoystickButton(ControlMap.DRIVER_LEFT, 2)
+                .onTrue(null);
+        //Floor Pickup Align
+        new JoystickButton(ControlMap.DRIVER_RIGHT, 2)
+                .onTrue(new GoToFull(ArmPositions.TIPPED_CONE_SETUP));
+        //Floor Pickup Collect
+        new JoystickButton(ControlMap.DRIVER_RIGHT, 2)
+                .onTrue(new GoToFull(ArmPositions.TIPPED_CONE));
+
+        //////END DRIVER 1//////////////////////////
+
+        //////CO DRIVER////////////////
+
+        //Substation Deploy
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 1)
+                .onTrue(null);
+        //Substation Collect
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 4)
+                .onTrue(null);
+        //Stow
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 6)
+                .onTrue(new GoToFull(ArmPositions.STORE));
+        //Cone Place High
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 7)
+                .onTrue(new GoToFull(ArmPositions.TOP_CONE));
+        //Cube Place High
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 8)
+                .onTrue(null);
+        //Cone Place Mid
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 9)
+                .onTrue(new GoToFull(ArmPositions.MIDDLE_CONE));
+        //Cube Place Mid
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 10)
+                .onTrue(null);
+        //Cone Place Low
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 11)
+                .onTrue(new GoToFull(ArmPositions.LOW));
+        //Cube Place Low
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 12)
+                .onTrue(new GoToFull(ArmPositions.LOW));
+        //Arm Override Enable
+        new JoystickButton(ControlMap.CO_DRIVER_BUTTONS, 13)
+                .whileTrue(null);
+
+        //////END CO-DRIVER//////////////////////////
     }
 }
--- src/main/java/frc/robot/subsystems/arm/ArmPositions.java
+++ src/main/java/frc/robot/subsystems/arm/ArmPositions.java
@@ -3,14 +3,13 @@ package frc.robot.subsystems.arm;
 public enum ArmPositions {
     TOP_CUBE(0,0,0),
     MIDDLE_CUBE(0,0,0),
-    TOP_CONE(0,0,0),
-    MIDDLE_CONE(0,0,0),
-    LOW(0,0,0),
+    TOP_CONE(0.783,3400/25,0.315),
+    MIDDLE_CONE(0.758,1091/25,0.315),
+    LOW(0.586,183/25,0.457),
     STORE(0,0,0),
     STATION(0,0,0),
     TIPPED_CONE(0,0,0),
-    TIPPED_CONE_SETUP(0,0,0),
-    FLOOR(0,0,0);
+    TIPPED_CONE_SETUP(0,0,0);
     
     
     public double angle,extension,wrist;
