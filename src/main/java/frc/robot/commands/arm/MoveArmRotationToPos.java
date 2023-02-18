d3d6e76394dd3f9693ddafefa4442e8c0d42e418
Alex Gardiner <99229221+AlexanderGardiner@users.noreply.github.com>
2023-02-17 20:22:29 -0800

    ArmMoveEverthingCommand

---
+++ src/main/java/frc/robot/commands/arm/MoveArmRotationToPos.java
@@ -0,0 +1,27 @@
+package frc.robot.commands.arm;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.arm.ArmExtension;
+import frc.robot.subsystems.arm.CaliGirls;
+import frc.robot.util.MathUtil;
+
+public class MoveArmRotationToPos extends CommandBase{
+    double target;
+
+    CaliGirls caliGirls;
+    public MoveArmRotationToPos(double pos) {
+        
+        this.target = pos;
+    }
+
+    @Override
+    public void initialize() {
+        this.caliGirls = CaliGirls.getInstance();
+        caliGirls.setBottomPos(target);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return (MathUtil.isWithinTolerance(caliGirls.getBottomPos(),target,0.5));
+    }
+}
---
+++ src/main/java/frc/robot/commands/arm/MoveExtensionToPos.java
@@ -0,0 +1,26 @@
+package frc.robot.commands.arm;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.arm.ArmExtension;
+import frc.robot.util.MathUtil;
+
+public class MoveExtensionToPos extends CommandBase{
+    double target;
+
+    ArmExtension armExtension;
+    public MoveExtensionToPos(double pos) {
+        
+        this.target = pos;
+    }
+
+    @Override
+    public void initialize() {
+        this.armExtension = ArmExtension.getInstance();
+        armExtension.SetPosition(target, false);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return (MathUtil.isWithinTolerance(armExtension.GetPosition(),target,0.5));
+    }
+}
