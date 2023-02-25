82c0fb3608bea7431a31b836fa923ba938b0976e
Lauren <laurenlee4655@gmail.com>
2023-02-24 18:22:32 -0800

    trig code

---
+++ src/main/java/frc/robot/util/ArmTrig.java
@@ -0,0 +1,26 @@
+package frc.robot.util;
+
+public class ArmTrig {
+    private double horizDistB;
+    private double vertDistB;
+    private double distE;
+    private double distC;
+    private double angleP;
+    private double angleW;
+
+    public ArmTrig (double horizDistB, double vertDistB, double distE, double distC, double angleP, double angleW) {
+        this.horizDistB = horizDistB;
+        this.vertDistB = vertDistB;
+        this.distE = distE;
+        this.distC = distC;
+        this.angleP = angleP;
+        this.angleW = angleW;
+    }
+
+    public double[] armTrig() {
+        horizDistB = distE*Math.cos(angleP) + distC*Math.cos(angleP + angleW);
+        vertDistB = distE*Math.sin(angleP) + distC*Math.sin(angleP + angleW);
+        double[] arr = {horizDistB, vertDistB};
+        return arr;
+    }
+}
