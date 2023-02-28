8e177871d25200bd59f8f86f19972b321cea5fdd
Anemone <zombiegam3r2o06@gmail.com>
2023-02-28 11:27:19 -0800

    Magic eject button

---
+++ src/main/java/frc/robot/commands/arm/Intake/SetItemMode.java
@@ -0,0 +1,12 @@
+package frc.robot.commands.arm.Intake;
+
+import java.util.Set;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.arm.Roller;
+
+public class SetItemMode extends InstantCommand{
+   public SetItemMode(boolean itemMode) {
+        Roller.getInstance().SetItemMode(itemMode);
+   } 
+}
---
+++ src/main/java/frc/robot/commands/arm/Intake/SmartEject.java
@@ -0,0 +1,22 @@
+package frc.robot.commands.arm.Intake;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.arm.CaliGirlsBottomMoveDownALittle;
+import frc.robot.subsystems.arm.Roller;
+
+public class SmartEject extends SequentialCommandGroup{
+    
+    private boolean itemMode;
+    private Roller roller;
+    
+    public SmartEject() {
+        this.roller = Roller.getInstance();
+        this.itemMode = roller.getItemMode();
+        if (itemMode) {
+            addCommands(new CaliGirlsBottomMoveDownALittle());
+        } else {
+            addCommands(new IntakeOut().withTimeout(2));
+        }
+    }
+
+}
