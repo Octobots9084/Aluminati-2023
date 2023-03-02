ef1bc0ae5b548d5820bb679de3bbef233c283159
Anemone <zombiegam3r2o06@gmail.com>
2023-03-01 18:39:43 -0800

    Driver assist

--- src/main/java/frc/robot/commands/swerve/SetDriveAngle.java
+++ src/main/java/frc/robot/commands/swerve/SetDriveAngle.java
@@ -14,43 +14,13 @@ import frc.robot.util.MathUtil;
 public class SetDriveAngle extends CommandBase {
     private final DriveTrain dt;
     private final double angle;
-    private double currentAngle;
-    private double angleDiff;
-    private double rotationSpeed;
     public SetDriveAngle(double angle) {
         this.dt = DriveTrain.getInstance();
         this.angle = angle;
-        this.currentAngle = Gyro.getInstance().getRotation2d().getDegrees();
-        this.angleDiff = angle-currentAngle;
     }
 
     @Override
     public void initialize() {
         dt.setTargetRotationAngle(angle);
     }
-
-    @Override
-    public boolean isFinished() {
-        return MathUtil.isWithinTolerance(currentAngle,angle,5);
-    }
-
-    @Override
-    public void execute() {
-        currentAngle = Gyro.getInstance().getRotation2d().getDegrees();
-        angleDiff = angle-currentAngle;
-        
-        rotationSpeed = 9 * angleDiff/180;
-        if (rotationSpeed>dt.MAX_ANGULAR_SPEED) {
-            rotationSpeed= dt.MAX_ANGULAR_SPEED;
-        }
-        dt.drive(0, 0, rotationSpeed, true);
-        dt.setTargetRotationAngle(Gyro.getInstance().getRotation2d().getDegrees());
-    }
-
-    @Override
-    public void end(boolean interrupted) {
-        dt.drive(0, 0, 0, false);
-        dt.setTargetRotationAngle(Gyro.getInstance().getRotation2d().getDegrees());
-    }
-
 }
