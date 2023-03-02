baf332ac705d870a8b0b80633f85a60c51628d40
Lauren <laurenlee4655@gmail.com>
2023-03-01 20:10:35 -0800

    set drive angle fixed

--- src/main/java/frc/robot/commands/swerve/SetDriveAngle.java
+++ src/main/java/frc/robot/commands/swerve/SetDriveAngle.java
@@ -1,17 +1,15 @@
 package frc.robot.commands.swerve;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.swerve.DriveTrain;
-import frc.robot.util.Gyro;
 import frc.robot.util.MathUtil;
 
 
 /**
  * Represents a swerve drive style drivetrain.
 */
-public class SetDriveAngle extends CommandBase {
+public class SetDriveAngle extends InstantCommand {
     private final DriveTrain dt;
     private final double angle;
     public SetDriveAngle(double angle) {
@@ -23,4 +21,5 @@ public class SetDriveAngle extends CommandBase {
     public void initialize() {
         dt.setTargetRotationAngle(angle);
     }
+
 }
