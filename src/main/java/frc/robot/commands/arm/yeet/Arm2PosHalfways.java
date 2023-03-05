632f7ec10357f49bdac5ff1839cfac6c44351e81
Anemone <zombiegam3r2o06@gmail.com>
2023-03-05 10:59:43 -0800

    fast arm commands

--- src/main/java/frc/robot/commands/arm/yeet/Arm2PosHalfways.java
+++ src/main/java/frc/robot/commands/arm/yeet/Arm2PosHalfways.java
@@ -2,31 +2,30 @@ package frc.robot.commands.arm.yeet;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.commands.arm.LowLevel.MoveArmExtensionToPos;
-import frc.robot.commands.arm.LowLevel.MoveArmRotationToPos;
-import frc.robot.commands.arm.LowLevel.MoveArmWristToPos;
-import frc.robot.commands.arm.LowLevel.SetArmAngle;
-import frc.robot.commands.arm.LowLevel.SetArmExtension;
-import frc.robot.commands.arm.LowLevel.SetWristAngle;
+import frc.robot.commands.arm.lowLevel.MoveArmExtensionToPos;
+import frc.robot.commands.arm.lowLevel.MoveArmRotationToPos;
+import frc.robot.commands.arm.lowLevel.MoveArmWristToPos;
+import frc.robot.commands.arm.lowLevel.SetArmAngle;
+import frc.robot.commands.arm.lowLevel.SetArmExtension;
+import frc.robot.commands.arm.lowLevel.SetWristAngle;
 import frc.robot.subsystems.arm.ArmExtension;
 import frc.robot.subsystems.arm.ArmPositions;
 import frc.robot.subsystems.arm.CaliGirls;
 
-public class Arm2PosCooldown extends SequentialCommandGroup {
+public class Arm2PosHalfways extends SequentialCommandGroup {
     ArmPositions aPosition;
     CaliGirls caliGirls;
     ArmExtension armExtension;
 
-    public Arm2PosCooldown(ArmPositions aPosition) {
+    public Arm2PosHalfways(ArmPositions aPosition) {
         this.aPosition = aPosition;
         this.caliGirls = CaliGirls.getInstance();
         this.armExtension = ArmExtension.getInstance();
         addCommands(
                 new SetWristAngle(aPosition.wrist),
-                new SetArmExtension(aPosition.extension),
-                new WaitCommand(0.3),
                 new SetArmAngle(ArmPositions.DRIVE_WITH_PIECE.armAngle, ArmPositions.DRIVE_WITH_PIECE.armAngle),
-                new Arm2PosStow(aPosition));
+                new WaitCommand(0.5),
+                new SetArmExtension(aPosition.extension));
 
     }
 }
