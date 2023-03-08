d257557d127ced75f21408785aeecf7430c1556c
Anemone <zombiegam3r2o06@gmail.com>
2023-03-08 10:35:30 -0800

    Imports optimized

--- src/main/java/frc/robot/commands/arm/intake/advanced/CubeInject.java
+++ src/main/java/frc/robot/commands/arm/intake/advanced/CubeInject.java
@@ -1,7 +1,6 @@
 package frc.robot.commands.arm.intake.advanced;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.arm.yeet.Arm2PosCooldown;
 import frc.robot.subsystems.arm.ArmPositions;
 
--- src/main/java/frc/robot/util/PoseFinder.java
+++ src/main/java/frc/robot/util/PoseFinder.java
@@ -1,7 +1,5 @@
 package frc.robot.util;
 
-import java.text.FieldPosition;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
