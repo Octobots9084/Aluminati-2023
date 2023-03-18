59b81a1e217937b1cd04fbf5f0e7b607ef36a92e
Coolkid0629 <90662954+Coolkid0629@users.noreply.github.com>
2023-03-18 15:36:57 -0700

    sim

--- src/main/java/frc/robot/commands/swerve/LogMetachromasia.java
+++ src/main/java/frc/robot/commands/swerve/LogMetachromasia.java
@@ -3,12 +3,13 @@ package frc.robot.commands.swerve;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.commands.swerve.SwerveControl;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class LogMetachromasia extends InstantCommand{
     @Override
     public void initialize() {
-        
-
-
         Double[] xSpeedsDouble = new Double[SwerveControl.xSpeeds.size()];
         xSpeedsDouble = SwerveControl.xSpeeds.toArray(xSpeedsDouble);
 
@@ -18,9 +19,24 @@ public class LogMetachromasia extends InstantCommand{
         Double[] rotsDouble = new Double[SwerveControl.rots.size()];
         rotsDouble = SwerveControl.rots.toArray(rotsDouble);
 
+        StringBuilder xString = new StringBuilder();
+        StringBuilder yString = new StringBuilder();
+        StringBuilder rotsString = new StringBuilder();
+
+        for (Double d : xSpeedsDouble) {
+            xString.append(' ').append(d.toString());
+        }
+        for (Double d : ySpeedsDouble) {
+            yString.append(' ').append(d.toString());
+        }
+        for (Double d : rotsDouble) {
+            rotsString.append(' ').append(d.toString());
+        }
+
+
         SmartDashboard.putNumber("test", 0);
-        SmartDashboard.putString("xSpeeds", xSpeedsDouble.toString());
-        SmartDashboard.putString("ySpeeds", ySpeedsDouble.toString());
-        SmartDashboard.putString("rot", rotsDouble.toString());
+        SmartDashboard.putString("xSpeeds", xString.toString());
+        SmartDashboard.putString("ySpeeds", yString.toString());
+        SmartDashboard.putString("rot", rotsString.toString());
     }
 }
