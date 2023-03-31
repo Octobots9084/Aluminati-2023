4b7ec2416c3170b3cc3c542b93134263a203e258
Alex Gardiner <99229221+AlexanderGardiner@users.noreply.github.com>
2023-03-30 21:05:05 -0700

    deadbands

--- src/main/java/frc/robot/commands/swerve/SwerveControl.java
+++ src/main/java/frc/robot/commands/swerve/SwerveControl.java
@@ -55,11 +55,11 @@ public class SwerveControl extends CommandBase {
 
 
         // Get speeds from joysticks
-        xSpeed = MathUtil.fitDeadband(-leftJoystick.getY()) * DriveTrain.MAX_TURN_SPEED;
-        ySpeed = MathUtil.fitDeadband(-leftJoystick.getX()) * DriveTrain.MAX_TURN_SPEED;
+        xSpeed = MathUtil.fitDeadband(-leftJoystick.getY(),0.02) * DriveTrain.MAX_TURN_SPEED;
+        ySpeed = MathUtil.fitDeadband(-leftJoystick.getX(),0.02) * DriveTrain.MAX_TURN_SPEED;
 
         // Calculate the deadband
-        rot = MathUtil.fitDeadband(-rightJoystick.getX()) * DriveTrain.MAX_ANGULAR_SPEED;
+        rot = MathUtil.fitDeadband(-rightJoystick.getX(),0.02) * DriveTrain.MAX_ANGULAR_SPEED;
         
         //just drive lol
         driveTrain.drive(xSpeed, ySpeed, rot, driveTrain.getFieldCentric());
