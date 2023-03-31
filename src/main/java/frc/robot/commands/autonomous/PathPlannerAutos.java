6236731bdedb1744095a9cac8a3532b50e7b328c
Alex Gardiner <99229221+AlexanderGardiner@users.noreply.github.com>
2023-03-31 09:09:22 -0700

    time testing

--- src/main/java/frc/robot/commands/autonomous/PathPlannerAutos.java
+++ src/main/java/frc/robot/commands/autonomous/PathPlannerAutos.java
@@ -86,7 +86,7 @@ public final class PathPlannerAutos {
     }
 
     public static CommandBase PlaceConeAndBalanceAndCommunity() {
-        return autoBuilder.fullAuto(PathPlanner.loadPathGroup("ChargeStationMoveAndGoOutsideCommunity", new PathConstraints(3, 2)));
+        return autoBuilder.fullAuto(PathPlanner.loadPathGroup("ChargeStationMoveAndGoOutsideCommunity", new PathConstraints(2, 1)));
     }
     public static CommandBase PlaceConeAndMoveBackBottom() {
         return autoBuilder.fullAuto(PathPlanner.loadPathGroup("MoveAndGrabConeBottom", new PathConstraints(2, 1)));
