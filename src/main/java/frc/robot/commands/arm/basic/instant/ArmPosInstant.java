b6dcc185e707ad96dc2659828d3b86577e1ba0cd
Lauren <laurenlee4655@gmail.com>
2023-04-04 19:59:59 -0700

    fix warnings

--- src/main/java/frc/robot/commands/arm/basic/instant/ArmPosInstant.java
+++ src/main/java/frc/robot/commands/arm/basic/instant/ArmPosInstant.java
@@ -1,9 +1,6 @@
 package frc.robot.commands.arm.basic.instant;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import frc.robot.commands.arm.basic.timed.ExtensionPosTimed;
-import frc.robot.commands.arm.basic.timed.CaliTopPosTimed;
-import frc.robot.commands.arm.basic.tolerance.CaliBottomPosTolerance;
 import frc.robot.subsystems.arm.ArmPositions;
 
 public class ArmPosInstant extends ParallelCommandGroup{
