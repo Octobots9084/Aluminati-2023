639fa7fbddf152a32f7dc8a49d0b5b038cfc578f
Anemone <zombiegam3r2o06@gmail.com>
2023-04-13 13:07:59 -0700

    substation collection fix untested

--- src/main/java/frc/robot/commands/advanced/CollectSubstation.java
+++ src/main/java/frc/robot/commands/advanced/CollectSubstation.java
@@ -2,11 +2,14 @@ package frc.robot.commands.advanced;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.arm.basic.instant.IntakeSpeedInstant;
+import frc.robot.commands.arm.basic.tolerance.CaliBottomPosTolerance;
+import frc.robot.commands.arm.basic.tolerance.ExtensionPosTolerance;
 import frc.robot.commands.arm.basic.instant.ArmPosInstant;
+import frc.robot.commands.arm.basic.instant.CaliTopPosInstant;
 import frc.robot.subsystems.arm.ArmPositions;
 public class CollectSubstation extends SequentialCommandGroup{
 
     public CollectSubstation() {
-        addCommands(new ArmPosInstant(ArmPositions.INTAKE_SUBSTATION), new IntakeSpeedInstant(-10));
+        addCommands(new ExtensionPosTolerance(0).withTimeout(0.1), new CaliTopPosInstant(ArmPositions.AUTO_ALIGN.wrist), new CaliBottomPosTolerance(0, 0, 0.1), new ArmPosInstant(ArmPositions.INTAKE_SUBSTATION), new IntakeSpeedInstant(-10));
     }
 }
