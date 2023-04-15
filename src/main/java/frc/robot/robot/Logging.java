7aa09b90954e5f51945178337f8148d070ef85e4
Anemone <zombiegam3r2o06@gmail.com>
2023-04-14 23:20:56 -0700

    Auto Naming is now awful but useful

--- src/main/java/frc/robot/robot/Logging.java
+++ src/main/java/frc/robot/robot/Logging.java
@@ -49,10 +49,10 @@ public class Logging {
     private void addAutoOptions() {
         //
         autoChooser.setDefaultOption("No Movement", new AutoConeTop());
-        autoChooser.addOption("BBCableSideBB", PathPlannerAutos.CableSideBlue2());
-        autoChooser.addOption("RRCableSideRR", PathPlannerAutos.CableSideRed());
-        autoChooser.addOption("RRCenterRR", PathPlannerAutos.DriveCollectChargeRed());
-        autoChooser.addOption("BBCenterBB", PathPlannerAutos.DriveCollectChargeBlue());
+        autoChooser.addOption("REDCableSide", PathPlannerAutos.CableSideBlue2());
+        autoChooser.addOption("CableSideBLUE", PathPlannerAutos.CableSideRed());
+        autoChooser.addOption("REDCenter", PathPlannerAutos.DriveCollectChargeRed());
+        autoChooser.addOption("CenterBLUE", PathPlannerAutos.DriveCollectChargeBlue());
 
         // autoChooser.addOption("DONNOTUSEBalance Charge Station", PathPlannerAutos.PlaceConeAndBalance());
         // autoChooser.addOption("Move and Grab Cone Bottom", PathPlannerAutos.PlaceConeAndMoveBackBottom());
